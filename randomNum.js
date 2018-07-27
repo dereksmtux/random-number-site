ee20a3f138d3905723847b64a911c17eb7139905
derek smith <dereksmith2112@verizon.net>
2018-07-27 00:42:17 -0500

    added number range feature

--- randomNum.js
+++ randomNum.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
   var card = $("<div class = 'row'> <div class = 'col-md-12 card'></div></div>");
 
   $(".Start").click(function(){
-    var userMax = prompt("What is your max number?", 10);
-    var userRuns = prompt("How many numbers do you want", 10);
+    var userMax = parseInt( prompt("What is your max number?", 10));
+    var userMin =  parseInt (prompt("What is your min number?", 1));
+    var userRuns = parseInt( prompt("How many numbers do you want", 10));
     
-    var userNums = run(userMax,userRuns);
+    var userNums = run(userMax,userMin, userRuns);
     
     $(".box").append(card);
     for(i = 0; i < userRuns; i ++){
@@ -30,15 +31,15 @@ $(document).ready(function(){
 // code for number generation
 
 //generates many numbers and places them in an array
-function run(max , runs){
+function run(max , min, runs){
   var list = [];
   for(i = 0; i< runs; i++){
-    list.push(randomNum(max));
+    list.push(randomNum(max, min));
   }
   return list;
 }
 //generates one number
-function randomNum(max){
-return Math.floor( Math.random() * max + 1);
+function randomNum(max, min){
+return Math.floor(  Math.random() * (max - min + 1)) + min;
 }
 
